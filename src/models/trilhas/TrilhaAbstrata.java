5befb9338bb1231486e9223e484a5c1ae2a1014b
Felipe Brito <arrudabritofelipe@gmail.com>
2023-07-09 19:00:38 -0300

    Exception for TrilhaAbstrata

--- src/models/trilhas/TrilhaAbstrata.java
+++ src/models/trilhas/TrilhaAbstrata.java
@@ -15,14 +15,26 @@ public abstract class TrilhaAbstrata implements ITrilha {
 	}
 
 	public TrilhaAbstrata(String nome, String descricao, Poder poderNex10, Poder poderNex40,
-			Poder poderNex65, Poder poderNex99) {
-		this.nome = nome;
-		this.descricao = descricao;
-		this.poderNex10 = poderNex10;
-		this.poderNex40 = poderNex40;
-		this.poderNex65 = poderNex65;
-		this.poderNex99 = poderNex99;
+	        Poder poderNex65, Poder poderNex99) {
+	    try {
+	        if (nome == null) {
+	            throw new IllegalArgumentException("O nome não pode ser nulo.");
+	        }
+	        if (descricao == null) {
+	            throw new IllegalArgumentException("A descrição não pode ser nula.");
+	        }
+
+	        this.nome = nome;
+	        this.descricao = descricao;
+	        this.poderNex10 = poderNex10;
+	        this.poderNex40 = poderNex40;
+	        this.poderNex65 = poderNex65;
+	        this.poderNex99 = poderNex99;
+	    } catch (IllegalArgumentException e) {
+	        System.err.println("Erro ao criar uma instância de TrilhaAbstrata: " + e.getMessage());
+	    }
 	}
+
 
 	public String getNome() {
 		return nome;
