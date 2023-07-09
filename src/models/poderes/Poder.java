c17b876aa73ba8591914d96c2546be5372e92b7d
Felipe Brito <arrudabritofelipe@gmail.com>
2023-07-09 19:01:08 -0300

    Exceptions for Poder

--- src/models/poderes/Poder.java
+++ src/models/poderes/Poder.java
@@ -9,25 +9,52 @@ public class Poder implements IPoder {
 	}
 
 	public Poder(String nome, String descricao) {
-		super();
-		this.nome = nome;
-		this.descricao = descricao;
+	    try {
+	        if (nome == null) {
+	            throw new IllegalArgumentException("O nome não pode ser nulo.");
+	        }
+	        if (descricao == null) {
+	            throw new IllegalArgumentException("A descrição não pode ser nula.");
+	        }
+
+	        this.nome = nome;
+	        this.descricao = descricao;
+	    } catch (IllegalArgumentException e) {
+	        System.err.println("Erro ao criar uma instância de Poder: " + e.getMessage());
+	    }
 	}
+
 
 	public String getNome() {
 		return nome;
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+	    try {
+	        if (nome == null) {
+	            throw new IllegalArgumentException("O nome não pode ser nulo.");
+	        }
+
+	        this.nome = nome;
+	    } catch (IllegalArgumentException e) {
+	        System.err.println("Erro ao definir o nome do Poder: " + e.getMessage());
+	    }
 	}
+
 
 	public String getDescricao() {
 		return descricao;
 	}
 
 	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
+	    try {
+	        if (descricao == null) {
+	            throw new IllegalArgumentException("A descrição não pode ser nula.");
+	        }
+
+	        this.descricao = descricao;
+	    } catch (IllegalArgumentException e) {
+	        System.err.println("Erro ao definir a descrição do Poder: " + e.getMessage());
+	    }
+	}	
 }
